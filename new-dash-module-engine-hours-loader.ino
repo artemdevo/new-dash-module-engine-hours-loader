413826c889b30b98357d404928702fd350998764
artemdevo <70590010+artemdevo@users.noreply.github.com>
2026-09-07 00:03:07 -0400

    Initial commit

---
+++ new-dash-module-engine-hours-loader.ino
@@ -0,0 +1,17 @@
+#include <EEPROM.h>
+#include <Arduino.h>
+
+const uint8_t ENGINE_HOURS_INTEGER_ADDRESS_0  4
+const uint8_t ENGINE_HOURS_INTEGER_ADDRESS_1 5
+const uint8_t ENGINE_HOURS_DECIMAL_ADDRESS_LOW 6
+const uint8_t ENGINE_HOURS_DECIMAL_ADDRESS_HIGH 25//using 20 addresses for the decimal portion
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
